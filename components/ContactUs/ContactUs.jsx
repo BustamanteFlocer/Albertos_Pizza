9c8fbcfccd85fce0feba1d713f927e53ad29b2e5
BustamanteFlocer <77340285+BustamanteFlocer@users.noreply.github.com>
2023-12-27 22:17:11 +0800

    edit

--- components/ContactUs/ContactUs.jsx
+++ components/ContactUs/ContactUs.jsx
@@ -1,78 +1,77 @@
 import styles from "@/components/ContactUs/ContactUs.module.css";
-import { HiOutlineMailOpen } from "react-icons/hi";
-import { FiMail, FiPhoneCall } from "react-icons/fi";
+import { IoIosMail } from "react-icons/io";
+import { FaLocationDot } from "react-icons/fa6";
+import { FaPhoneAlt } from "react-icons/fa";
 import AOS from "aos";
 import "aos/dist/aos.css";
 import React, { useEffect } from "react";
 
 const ContactUs = () => {
   useEffect(() => {
-    AOS.init();
+    AOS.init({ duration: 2000 });
   }, []);
   return (
     <div className={styles.contain}>
-      <div className={styles.container}>
-        <div className={styles.box1} data-aos="fade-right">
-          <div className={styles.profile}>
-            <h1 className={styles.header}>Send Us A Message</h1>
-            <div className={styles.textWrapper}>
-              <div className={styles.textBox}></div>
-              <div className={styles.address}>
-                <h2>Address:</h2>
-                <p>1030 Southwood Dr San Luis Obispo, California(CA), 93401</p>
+      <div className={styles.wrapper}>
+        <div className={`${styles.containerFluid} ${styles.contact}`}>
+          <div className={`${styles.contactContainer} ${styles.row} `}>
+            <div className={styles.contactRounded}>
+              <div className={styles.contactForm}>
+                <h1 data-aos="fade-up">Get in touch</h1>
+                <p data-aos="fade-up" data-aos-delay="300">
+                  Craving an extraordinary pizza experience that transcends the
+                  ordinary? Look no further than Alberto's Pizza, where every
+                  slice is a masterpiece! Dial, click, or tap your way to pizza
+                  delight – Alberto's is just a touch away from turning your
+                  cravings into a flavorful experience!
+                </p>
               </div>
-              <div className={styles.textBox}></div>
-              <div className={styles.links}>
-                <h2>Contact:</h2>
+            </div>
+
+            <div className={styles.mapContainer}>
+              <iframe
+                src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d981.3526442090455!2d123.8906106!3d10.3089892!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x33a9994dde515555%3A0xf9e837c1d36fb6cc!2sAlberto&#39;s%20Pizza!5e0!3m2!1sen!2sph!4v1703507321310!5m2!1sen!2sph"
+                loading="lazy"
+                referrerpolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
+
+            <div className={styles.contactInput}>
+              <form action="" className="">
+                <input type="text" placeholder=" Your Name"></input>
+                <input type="email" placeholder=" Enter Your Email"></input>
+                <textarea
+                  rows="5"
+                  cols="10"
+                  placeholder="Your Message"
+                ></textarea>
+                <button type="submit">Submit</button>
+              </form>
+            </div>
+
+            <div className={styles.contactInfoWrapper}>
+              <div className={styles.contactInfo}>
+                <FaLocationDot className={styles.icons} />
                 <div>
-                  <span>
-                    <FiPhoneCall className={styles.linkIcon} />
-                  </span>
-                  <a href="tel:+4733378901">+47 333 78 901</a>
+                  <h4>Address</h4>
+                  <p>15 B. Rodriguez St, Cebu City</p>
                 </div>
+              </div>
+              <div className={styles.contactInfo}>
+                <IoIosMail className={styles.icons} />
                 <div>
-                  <span>
-                    <HiOutlineMailOpen className={styles.linkIcon} />
-                  </span>
-                  <a href="mailto:miladamiri@gmail.com">miladamiri@gmail.com</a>
+                  <h4>Mail Us</h4>
+                  <p>albertospizza@gmail.com</p>
+                </div>
+              </div>
+              <div className={styles.contactInfo}>
+                <FaPhoneAlt className={styles.icons} />
+                <div>
+                  <h4>Telephone</h4>
+                  <p>(+032) 231 4628</p>
                 </div>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div className={styles.box2}>
-          <div></div>
-          <div id={styles.formWrap} className={styles.formWrap}>
-            <form action="">
-              <label className={styles.message} htmlFor="message">
-                message:
-              </label>
-              <textarea
-                name="message"
-                id={styles.message}
-                cols="30"
-                rows="10"
-                placeholder="Your message"
-              ></textarea>
-
-              <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                name="name"
-                id="name"
-                placeholder="Your name"
-              />
-              <label htmlFor="email">Email:</label>
-              <input
-                type="text"
-                name="email"
-                id="email"
-                placeholder="Your email"
-              />
-
-              <input type="submit" name="submit" value="Send" />
-            </form>
           </div>
         </div>
       </div>
